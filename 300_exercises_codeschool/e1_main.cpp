e6de2f8d1a1087381c7b5c6ba46fcd84a7d6e2af
NaiPret <ngvphu235e@gmail.com>
2023-07-19 21:57:40 +0700

    doi ten project cho dep:>

---
+++ 300_exercises_codeschool/e1_main.cpp
@@ -0,0 +1,15 @@
+#include <cmath>  //to use the pi number M_PI
+#include <iostream>
+
+int main() {
+    //* dien tich: area
+    //* the tich: volume = 4/3𝝅r3
+    //* ban kinh: radius
+    std::cout << "Nhap dien tich S: ";
+    double area{};
+    std::cin >> area;
+    double radius{sqrt(area / (4 * M_PI))};  // area = 4𝝅r2
+    std::cout << "The tich V = " << 4.0 / 3 * M_PI * pow(radius, 3)
+              << std::endl;
+    return 0;
+}
