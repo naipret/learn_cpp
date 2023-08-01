80ff2283a79b83ae71a766bf291dbe09ec754dee
NaiPret <ngvphu235e@gmail.com>
2023-08-01 15:57:51 +0700

    do ex6

--- 300_exercises_codeschool/e6_main.cpp
+++ 300_exercises_codeschool/e6_main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
-void bubbleSort(int input_arr[]) {
-    for (int i = 0; i < 3; ++i) {
-        for (int j = i + 1; j < 3; ++j) {
-            if (input_arr[i] > input_arr[j]) {
-                std::swap(input_arr[i], input_arr[j]);
-            }
-        }
-    }
-}
+#include "..\include\sorting_algorithms.h"
+
+// void bubbleSort(int input_arr[]) {
+//     for (int i = 0; i < 3; ++i) {
+//         for (int j = i + 1; j < 3; ++j) {
+//             if (input_arr[i] > input_arr[j]) {
+//                 std::swap(input_arr[i], input_arr[j]);
+//             }
+//         }
+//     }
+// }
 
 int main() {
-    int input_arr[3] {};
+    int input_arr[3]{};
     std::cin >> input_arr[0] >> input_arr[1] >> input_arr[2];
     bubbleSort(input_arr);
     std::cout << "Tang dan: ";
--- src/sorting_algorithms.cpp
+++ src/sorting_algorithms.cpp
@@ -1,2 +1,13 @@
 #include "..\include\sorting_algorithms.h"
 
+#include <iostream>
+
+void bubbleSort(int input_arr[]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = i + 1; j < 3; ++j) {
+            if (input_arr[i] > input_arr[j]) {
+                std::swap(input_arr[i], input_arr[j]);
+            }
+        }
+    }
+}
