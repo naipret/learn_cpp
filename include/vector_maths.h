f94c61ffa08cab5416402d3abcaaf361597d8e04
NaiPret <ngvphu235e@gmail.com>
2023-08-06 21:59:29 +0700

    git rm -r --cached .

--- include/vector_maths.h
+++
@@ -1,7 +0,0 @@
-#ifndef VECTOR_MATHS_H
-#define VECTOR_MATHS_H
-
-double onePointVectorLength(const double &xM, const double &yM);
-double twoPointVectorLength(const double &xM, const double &yM, const double &xN, const double &yN);
-
-#endif /* VECTOR_MATHS_H */
