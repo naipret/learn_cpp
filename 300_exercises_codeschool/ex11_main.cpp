58b0f5a00b4d25079acc18222113d6f4f78342d6
NaiPret <ngvphu235e@gmail.com>
2023-08-06 15:31:19 +0700

    ubuntu ko nhan cap nhat

---
+++ 300_exercises_codeschool/ex11_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int humanScore{0}, computerScore{0};
+    std::string humanInput{}, computerInput{};
+    while (true) {
+        std::cout << "Nhap ky tu (keo / bua / bao), ky tu khac de thoat" << std::endl;
+        std::cout << "Human: ";
+        std::cin >> humanInput;
+        
+    }
+    return 0;
+}
