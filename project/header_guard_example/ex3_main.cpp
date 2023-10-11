ece834be994ad21a0772b8bbf6bd2c3a6a62464d
NaiPret <ngvphu235e@gmail.com>
2023-10-11 21:29:46 +0700

    add new project "header guard example"

---
+++ project/header_guard_example/ex3_main.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iostream>
+
+#include "../../include/vector_maths.hpp"
+
+// double vector_length(double xI, double yI, double xM, double yM) {
+//     return sqrt(pow(xM - xI, 2) + pow(yM - yI, 2));
+// }
+
+int main() {
+    double xC{}, yC{}, R{}, xM{}, yM{};
+    std::cout << "Nhap toa do tam C(xC, yC)? ";
+    std::cin >> xC >> yC;
+    std::cout << "Nhap ban kinh R? ";
+    std::cin >> R;
+    std::cout << "Nhap toa do diem M(xM, yM)? ";
+    std::cin >> xM >> yM;
+    double IM{twoPointVectorLength(xC, yC, xM, yM)};
+    if (R == IM)
+        std::cout << "M nam tren C()" << std::endl;
+    else if (R > IM)
+        std::cout << "M nam trong C()" << std::endl;
+    else if (R < IM)
+        std::cout << "M nam ngoai C()" << std::endl;
+    return 0;
+}
