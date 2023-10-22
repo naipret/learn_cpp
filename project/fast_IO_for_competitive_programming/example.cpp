50b9380bfff5aef275add20f2a6d5f6ac3451104
NaiPret <ngvphu235e@gmail.com>
2023-10-22 09:19:04 +0700

    add project\fast_IO_for_competitive_programming

---
+++ project/fast_IO_for_competitive_programming/example.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define FASTER                        \
+    std::ios::sync_with_stdio(false); \
+    std::cout.tie(NULL);              \
+    std::cin.tie(NULL);
+
+using namespace std;
+
+int main() {
+    FASTER;
+    
+    return 0;
+}
