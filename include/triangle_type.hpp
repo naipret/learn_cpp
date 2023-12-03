0554e567a84790ca78b48e6d8f22da3e0a013300
NaiPret <ngvphu235e@gmail.com>
2023-12-03 18:24:45 +0700

    rewrite hpp

--- include/triangle_type.hpp
+++ include/triangle_type.hpp
@@ -1,9 +1,9 @@
-#ifndef TRIANGLE_TYPE_H
-#define TRIANGLE_TYPE_H
+#ifndef TRIANGLE_TYPE_HPP
+#define TRIANGLE_TYPE_HPP
 
 #include <iostream>
 
 inline std::string triangleType(const double &a, const double &b, const double &c);
 inline std::string advancedTriangleType(const double &a, const double &b, const double &c);
 
-#endif /* TRIANGLE_TYPE_H */
+#endif /* TRIANGLE_TYPE_HPP */
