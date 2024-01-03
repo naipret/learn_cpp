639cd3a6b4fe55f633457870bc13ebfadf729aa9
NaiPret <ngvphu235e@gmail.com>
2024-01-03 13:08:22 +0700

    thêm 2 file template lập trình thi đấu

---
+++ project/optimize_cpp/ffmain.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ifstream input{};
+    input.open("");
+    
+    input.close();
+
+    
+
+    ofstream output{};
+    output.open("");
+    
+    output.close();
+
+    return 0;
+}
