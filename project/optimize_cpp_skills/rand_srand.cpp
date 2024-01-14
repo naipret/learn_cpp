eb27419f6aa919955f0695f31b469e4ce47a5937
NaiPret <ngvphu235e@gmail.com>
2024-01-14 18:20:47 +0700

    viết lại phần so sánh sử dụng srand và <random>

--- project/optimize_cpp_skills/rand_srand.cpp
+++ project/optimize_cpp_skills/rand_srand.cpp
@@ -1,22 +1,25 @@
+#include <chrono>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 
 int main() {
-    constexpr int iterations = 1000000000;  // Số lần lặp
+    constexpr int iterations{1000000};  // Số lần lặp
+    std::cout << "So lan thuc hien: " << iterations << "\n";
 
     std::srand(std::time(0));
 
-    clock_t start_time = clock();  // Thời điểm bắt đầu
+    auto start_time{std::chrono::high_resolution_clock::now()};
 
-    for (int i = 0; i < iterations; ++i) {
-        int randomNum = std::rand();
+    for (int i{0}; i < iterations; ++i) {
+        int randomNum{std::rand()};
         // Sử dụng randomNum để tránh việc compiler loại bỏ mã không sử dụng
     }
 
-    clock_t end_time = clock();  // Thời điểm kết thúc
+    auto end_time{std::chrono::high_resolution_clock::now()};
+    auto duration{std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time)};
 
-    std::cout << "Time using rand(): " << double(end_time - start_time) / CLOCKS_PER_SEC << " seconds." << std::endl;
+    std::cout << "Time using rand(): " << duration.count() << " seconds." << std::endl;
 
     return 0;
 }
--- project/optimize_cpp_skills/random.cpp
+++ project/optimize_cpp_skills/random.cpp
@@ -1,23 +1,26 @@
+#include <chrono>
 #include <iostream>
 #include <random>
 
 int main() {
-    constexpr int iterations = 1000000000;  // Số lần lặp
+    constexpr int iterations{1000000};  // Số lần lặp
+    std::cout << "So lan thuc hien: " << iterations << "\n";
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(1, 100);
 
-    clock_t start_time = clock();  // Thời điểm bắt đầu
+    auto start_time{std::chrono::high_resolution_clock::now()};
 
-    for (int i = 0; i < iterations; ++i) {
-        int randomNum = dis(gen);
+    for (int i{0}; i < iterations; ++i) {
+        int randomNum{dis(gen)};
         // Sử dụng randomNum để tránh việc compiler loại bỏ mã không sử dụng
     }
 
-    clock_t end_time = clock();  // Thời điểm kết thúc
+    auto end_time{std::chrono::high_resolution_clock::now()};
+    auto duration{std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time)};
 
-    std::cout << "Time using <random>: " << double(end_time - start_time) / CLOCKS_PER_SEC << " seconds." << std::endl;
+    std::cout << "Time using <random>: " << duration.count() << " seconds." << std::endl;
 
     return 0;
 }
