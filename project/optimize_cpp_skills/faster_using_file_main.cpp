fb7ae88086f173366978cf0fab0123cd738da3a7
NaiPret <ngvphu235e@gmail.com>
2024-05-21 06:58:49 +0700

    update fstream with buffer

--- project/optimize_cpp_skills/faster_using_file_main.cpp
+++ project/optimize_cpp_skills/faster_using_file_main.cpp
@@ -1,27 +1,23 @@
 #include <fstream>
-#include <ios>
 
-// using namespace std;
+using namespace std;
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    constexpr std::streamsize buffer_size{4096};
-    char buffer[buffer_size];
+    ios_base::sync_with_stdio(false);
 
-    
+    constexpr streamsize bufferSize{8 * 1024};
+    char buffer[bufferSize];
 
-    std::ifstream input{};
-    input.rdbuf()->pubsetbuf(buffer, buffer_size);
-    input.open("", std::ios::binary);
-    
+    ifstream input{};
+    input.rdbuf()->pubsetbuf(buffer, bufferSize);
+    input.open("input.inp", ios_base::binary);
+
     input.close();
 
-    
+    ofstream output{};
+    output.rdbuf()->pubsetbuf(buffer, bufferSize);
+    output.open("output.out", ios_base::binary);
 
-    std::ofstream output{};
-    output.rdbuf()->pubsetbuf(buffer, buffer_size);
-    output.open("", std::ios::binary);
-    
     output.close();
 
     return 0;
